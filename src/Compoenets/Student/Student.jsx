fc046284fcf6549902b1268ed9b042a6665cf5f9
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-06 14:17:58 -0600

    Section 3 - Class Components

--- src/Compoenets/Student/Student.jsx
+++ src/Compoenets/Student/Student.jsx
@@ -1,19 +1,23 @@
-const Student = (props) => {
-  return (
-    <div className="col-4 p-1">
-      <div className="row border">
-        <div className="col-2">
-          <img src={props.headshot} className="w-100 py-2"></img>
+import React from "react";
+
+class Student extends React.Component {
+  render() {
+    return (
+      <div className="col-4 p-1">
+        <div className="row border">
+          <div className="col-2">
+            <img src={this.props.headshot} className="w-100 py-2"></img>
+          </div>
+          <div className="col-8">
+            {this.props.name}
+            <br />
+            Coding Experience {this.props.experience} years
+          </div>
+          <div className="col-2">{this.props.children}</div>
         </div>
-        <div className="col-8">
-          {props.name}
-          <br />
-          Coding Experience {props.experience} years
-        </div>
-        <div className="col-2">{props.children}</div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Student;
--- src/Compoenets/Student/StudentReview.jsx
+++ src/Compoenets/Student/StudentReview.jsx
@@ -1,16 +1,20 @@
-const StudentReview = () => {
-  return (
-    <div className="p-2">
-      <i
-        class="bi bi-hand-thumbs-up-fill text-success p-1"
-        style={{ cursor: "pointer" }}
-      ></i>
-      <i
-        class="bi bi-hand-thumbs-down-fill text-danger p-1"
-        style={{ cursor: "pointer" }}
-      ></i>
-    </div>
-  );
-};
+import React from "react";
+
+class StudentReview extends React.Component {
+  render() {
+    return (
+      <div className="p-2">
+        <i
+          class="bi bi-hand-thumbs-up-fill text-success p-1"
+          style={{ cursor: "pointer" }}
+        ></i>
+        <i
+          class="bi bi-hand-thumbs-down-fill text-danger p-1"
+          style={{ cursor: "pointer" }}
+        ></i>
+      </div>
+    );
+  }
+}
 
 export default StudentReview;
