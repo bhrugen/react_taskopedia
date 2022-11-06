8861bb043b952f524d70227d4320edad7edd42e9
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-06 08:05:03 -0600

    Section 3 - Student Component

--- src/index.js
+++ src/index.js
@@ -45,6 +45,30 @@ function MainBody() {
     </div>
   );
 }
+
+function Students() {
+  const fullName = "Kris Walley";
+  const programmingExp = 2;
+  return (
+    <div className="container p-4">
+      <div className="row">Students Enrolled</div>
+      <div className="row border">
+        <div className="col-2">
+          <img
+            src={`https://ui-avatars.com/api/?name=${fullName}`}
+            className="w-100"
+          ></img>
+        </div>
+        <div className="col-10">
+          {fullName}
+          <br />
+          Coding Experience {programmingExp} years
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function Footer() {
   return (
     <p style={{ color: "gray", backgroundColor: "black" }}>Happy Coding!</p>
@@ -54,6 +78,7 @@ root.render(
   <div>
     <Header />
     <MainBody />
+    <Students />
     <Footer />
   </div>
 );
