9d2f0c04d4dbb1c19cc2c58a68c417028578d279
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-06 07:48:58 -0600

    Section 3 - HTML Element in JSX

--- src/index.js
+++ src/index.js
@@ -26,16 +26,22 @@ function Header() {
 
 function MainBody() {
   const whatWeWillLearn = "React JS";
-
+  const lectureCount = 3;
   return (
     <div>
       <p>
-        In this course, we will learn {whatWeWillLearn} by building TaskOPedia!
+        In this course, we will learn {whatWeWillLearn} by building TaskOPedia!{" "}
+        <br />
+        Total Lecture - {lectureCount}
       </p>
       <ul>
         <li>Basic Foundation</li>
         <li>Functional and Class Components</li>
       </ul>
+      {/* <div>
+        Enter Task :{" "}
+        <input maxLength={5} readOnly={false} placeholder="Ben"></input>
+      </div> */}
     </div>
   );
 }
